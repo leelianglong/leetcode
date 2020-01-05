395d33216dbd5d3a6b1bdedd739a6f6e136211f3
lilianglong <lilianglong@nudt.edu.cn>
2020-01-05 23:08:02 +0800

    新增二叉树的其他几种遍历方法

--- binary_tree.c
+++ binary_tree.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
+#include <math.h>
 
 /****************** 二叉树结构定义 ****************/
 typedef struct BTNode {
@@ -10,13 +12,25 @@ typedef struct BTNode {
 } BinaryNode, *BinaryTree;
 /****************** 二叉树结构定义 ****************/
 
-/****************** 栈的结构定义 ****************/
+/****************** 栈的结构定义 *****************/
 #define  STATCK_LENGTH           1000
 typedef struct {
     int top;
     BinaryTree* data; /* 这里需要使用二叉树指针的指针 */
-}Stack;
-/****************** 栈的结构定义 ****************/
+} Stack;
+/****************** 栈的结构定义 *****************/
+
+/****************** 队列结构定义 *****************/
+typedef struct QNode {
+    BinaryTree* data;
+    struct QNode* next;
+} QueueNode,*QueueList;
+
+typedef struct LQ {
+    QueueList front, rear; // 对头插入，队尾删除
+} LinkQueue;
+/****************** 队列结构定义 *****************/
+
 /***************************************栈基本操作**************************************/
 bool InitStack(Stack* para)
 {
@@ -37,7 +51,7 @@ bool InitStack(Stack* para)
 一开始top在最底段，每次入栈，top先增加，把元素放到栈顶。
 出栈时，先从栈顶拿出元素，然后栈顶指针top再减减。
 */
-bool PushStack(Stack* para, BinaryNode data)
+bool PushStack(Stack* para, BinaryTree data)
 {
     if (para == NULL) {
         return false;
@@ -49,7 +63,7 @@ bool PushStack(Stack* para, BinaryNode data)
     return true;
 }
 
-bool PopStack(Stack* para, BinaryTree data)
+bool PopStack(Stack* para, BinaryTree* data)
 {
     if (para == NULL) {
         return false;
@@ -66,6 +80,76 @@ bool CheckStackIsEmpty(Stack* para)
     return para->top == -1 ? true : false;
 }
 /***************************************栈基本操作**************************************/
+
+/**************************************队列基本操作**************************************/
+bool InitQueue(LinkQueue* lq)
+{
+    lq->front = lq->rear = (QueueList)malloc(sizeof(QueueNode));
+    if (lq->front == NULL) {
+        return false;
+    }
+    lq->front->next = NULL;
+    return true;
+}
+
+void DestoryQueue(LinkQueue* lq)
+{
+    while (lq->front) {
+        lq->rear = lq->front->next;
+        free(lq->front);
+        lq->front = lq->rear;
+    }
+}
+
+bool QueueIsEmpty(LinkQueue lq)
+{
+    return lq.rear == lq.front ? true : false;
+}
+
+int GetQueueLength(LinkQueue lq)
+{
+    int i = 0;
+    QueueList p = lq.front;
+    while (lq.rear != p) {
+        i++;
+        p = p->next;
+    }
+    return i;
+}
+
+bool InsertQueue(LinkQueue* lq, BinaryTree data)
+{
+    QueueList p;
+    p = (QueueList)malloc(sizeof(QueueNode));
+    if (p == NULL) {
+        return false; 
+    }
+    p->data = data;
+    p->next = NULL;
+    lq->rear->next = p; //起初队尾节点的下一个是null，这里新创建一个节点就接上。
+    lq->rear = p; //新创建的节点变成队尾。
+    return true;
+}
+
+bool DelQueue(LinkQueue* lq, BinaryTree* data)
+{
+    QueueList p;
+    if (lq->rear == lq->front) {
+        return false;
+    }
+    p = lq->front->next;
+    *data = p->data;
+    lq->front->next = p->next;
+    if (lq->rear == p) {
+        lq->rear = lq->front;
+    }
+    free(p);
+    return true;
+}
+/**************************************队列基本操作**************************************/
+
+
+
 
 /* 
 创建二二叉树：
@@ -195,16 +279,44 @@ int GetLeafCounter(BinaryTree bt) {
 }
 
 /*
-二叉树的后序遍历(递归版本)
+统计二叉树所有节点个数
 */
-void PostOrderTraverse(BinaryTree bt, int* saveData, int* counter)
+int GetNodeCounter(BinaryTree bt)
+{
+    int leftHigh = 0;
+    int rightHigh = 0;
+    BinaryTree left = bt;
+    BinaryTree right = bt;
+    if (bt == NULL) {
+        return 0;
+    }
+    while (left) {
+        left = left->lchild;
+        leftHigh++;
+    }
+
+    while (right) {
+        right = right->rchild;
+        rightHigh++;
+    }
+
+    if (leftHigh == rightHigh) {
+        return (int)(1 << rightHigh) - 1; // 左右子树一样高，是满二叉树，节点个数 = 2^n - 1;
+    } 
+    return 1 + GetNodeCounter(bt->lchild) + GetNodeCounter(bt->rchild);
+}
+
+/*
+前序遍历，递归版本
+*/
+void PreOrderTraverse(BinaryTree bt, int* saveData, int* counter)
 {
     if (bt == NULL) {
         return ;
     }
+    saveData[(*counter)++] = bt->data;   
     PreOrderTraverse(bt->lchild, saveData, counter);
     PreOrderTraverse(bt->rchild, saveData, counter);
-    saveData[(*counter)++] = bt->data;
 }
 
 /*
@@ -234,7 +346,6 @@ int* PreOrderTraverseByStack(BinaryTree bt, int* counter)
     return saveData;
 }
 
-
 /*
 二叉树的中序遍历(递归版本)
 */
@@ -243,9 +354,9 @@ void MiddleOrderTraverse(BinaryTree bt, int* saveData, int* counter)
     if (bt == NULL) {
         return ;
     }
-    PreOrderTraverse(bt->lchild, saveData, counter);
+    MiddleOrderTraverse(bt->lchild, saveData, counter);
     saveData[(*counter)++] = bt->data;
-    PreOrderTraverse(bt->rchild, saveData, counter);
+    MiddleOrderTraverse(bt->rchild, saveData, counter);
 }
 
 /**
@@ -283,8 +394,8 @@ void PostOrderTraverse(BinaryTree bt, int* saveData, int* counter)
     if (bt == NULL) {
         return ;
     }
-    PreOrderTraverse(bt->lchild, saveData, counter);
-    PreOrderTraverse(bt->rchild, saveData, counter);
+    PostOrderTraverse(bt->lchild, saveData, counter);
+    PostOrderTraverse(bt->rchild, saveData, counter);
     saveData[(*counter)++] = bt->data;
 }
 
@@ -295,12 +406,11 @@ void PostOrderTraverse(BinaryTree bt, int* saveData, int* counter)
    问的节点p是否为栈顶节点的左子树
 3、如果是的话那么还需要访问栈顶节点的右子树，所以将栈顶节点的右子树取出赋值给p
 4、如果不是的话则说明栈顶节点的右子树已经访问完了，那么现在可以访问栈顶节点了，所以此时将p赋值为null
-https://blog.csdn.net/YEYUANGEN/article/details/26363235
 *******************************************************************************/
 int* PostOrderTraversByStack(BinaryTree bt, int* counter)
 {
     Stack binaryTree = {0};
-    BinaryTree haveAccess = NULL;
+    BinaryTree haveAccess = NULL; // 用来标记右子树是否访问到了，在访问到的情况下，才能访问根节点。
     int* saveData = (int*)malloc(STATCK_LENGTH * sizeof(int));
     if (saveData == NULL) {
         return NULL;
@@ -310,7 +420,7 @@ int* PostOrderTraversByStack(BinaryTree bt, int* counter)
     while (bt != NULL || binaryTree.top != -1)
     {
         while (bt != NULL) { // 遍历完所有左子树
-            PushStack(&binaryTree,bt);
+            PushStack(&binaryTree, bt);
             bt = bt->lchild;
         }
         PopStack(&binaryTree, &bt);
@@ -319,26 +429,70 @@ int* PostOrderTraversByStack(BinaryTree bt, int* counter)
             haveAccess = bt;
             bt = NULL;
         } else {
+            PushStack(&binaryTree, bt); //在回到根节点时，再次把根节点入栈
             bt = bt->rchild;
         }
     }
     return saveData;
 }
 
+
+/*
+二叉树的层次遍历
+*/
+int* LevelTraverse(BinaryTree bt, int* counter)
+{
+    int* res = (int*)malloc(STATCK_LENGTH * sizeof(int));
+    if (res == NULL) {
+        return NULL;
+    }
+    LinkQueue lq = {0};
+    InitQueue(&lq);
+    InsertQueue(&lq, bt);
+    while (GetQueueLength(lq)) {
+        DelQueue(&lq, &bt);
+        res[(*counter)++] = bt->data;
+        if (bt->lchild) {
+            InsertQueue(&lq, bt->lchild);
+        }
+        if (bt->rchild) {
+            InsertQueue(&lq, bt->rchild);
+        }
+    }
+    return res;
+}
+
 int main(void)
 {
     int data[] = {1,2,4,999,999,5,999,999,3,6,999,999,7,999,999,999,999,999,999,999};
     int counter = 0;
+    int i;
     int saveData[10] = {0};
     BinaryTree root = NULL;
     int accessCounter = 0;
     int* result = NULL;
 
     CreatBinary(&root, data, &counter);
+    PreOrderTraverse(root, saveData, &accessCounter);
+    printf("\r\n前序遍历：递归\r\n");
+    for(i = 0; i < accessCounter; i++) {
+        printf("%4d", saveData[i]);
+    }
     int deepth = GetBinaryTreeDeepth(root);
     int leafCount = GetLeafCounter(root);
+    accessCounter = 0;
     result = PostOrderTraversByStack(root, &accessCounter);
-    for (int i = 0; i < accessCounter; ++i) {
+    printf("\r\n前序遍历：非递归\r\n");
+    for (i = 0; i < accessCounter; ++i) {
+        printf("%4d", result[i]);
+    }
+    counter = GetNodeCounter(root);
+    printf("\r\nnode is %u\r\n", counter);
+
+    printf("\r\nlevel traverse\r\n");
+    counter = 0;
+    result = LevelTraverse(root, &counter);
+    for (i = 0; i < counter; ++i) {
         printf("%4d", result[i]);
     }
     system("pause");
