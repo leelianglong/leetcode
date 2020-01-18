4db2b1ec69d040c9398fffc274962791e5ba7055
lilianglong <lilianglong@nudt.edu.cn>
2020-01-18 10:29:46 +0800

    增加返回二维数组的常规方法

---
+++ 一些算法收集/二维数组.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ALLOC_SIZE   1000
+
+int** ReturnTwoDimensionArray(int* returnSize, int** returnColSize)
+{
+    int i;
+    int j;
+    int counter = 0;
+    int** res = (int**)malloc(ALLOC_SIZE * sizeof(int));
+    *returnColSize = (int*)malloc(ALLOC_SIZE * sizeof(int));
+
+    for (i = 0; i < ALLOC_SIZE; i++) {
+        #if 1   //这里2种分配内存的并赋值的方法都正确。
+        res[(*returnSize)++] = (int*)malloc((i + 1) * sizeof(int));
+        (*returnColSize)[i] = (i + 1);
+        #else
+        *(res + (*returnSize)) = (int*)malloc((i + 1) * sizeof(int));
+        *((*returnColSize) + i) = (i + 1);
+        #endif
+        for (j = 0; j < (i + 1); j++) {
+            *(*(res + (*returnSize)) + j) = counter++;
+            printf("%4d", *(*(res + (*returnSize)) + j));
+        }
+        (*returnSize)++;
+        printf("\r\n");
+    }
+    return res;
+}
+
+
+int main(void)
+{
+    int i;
+    int j;
+    int num = 0;
+#if 1  
+    int returnSize = 0;
+    int (*colArra)[ALLOC_SIZE] = NULL; //关键是这里，定义成一个指针，指向一个数组，这里取地址传入，在下面的函数中会分配内存给这个指针。
+    int **result = ReturnTwoDimensionArray(&returnSize, &colArra);
+    for (i = 0; i < returnSize; i++) {
+        printf("\r\ncol = %4d\r\n", (*colArra)[i]);
+        for (j = 0; j < (*colArra)[i]; j++) {
+            printf("%4d", *(*(result + i) + j));
+        }
+        printf("\r\n");
+    }
+#endif
+    system("pause");
+}
