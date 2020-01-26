b1a363cd668d9fb9a7f2ed91eec1584c578b26af
lilianglong <lilianglong@nudt.edu.cn>
2020-01-26 22:14:46 +0800

    增加第二题的解答

--- leetcode_2.c
+++ leetcode_2.c
@@ -1,10 +1,11 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
 
 #define  STACK_LENGTH     100
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
@@ -20,6 +21,8 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
     if (l1 == NULL || l2 == NULL) {
         return NULL;
     }
+
+    //节点入栈
     while(l1) {
         stack1[++top1] = l1->val;
         l1 = l1->next;
@@ -29,6 +32,20 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
         l2 = l2->next;
     }
 
+    //下面处理2个栈不一样深的话，那么把其中一个使用0补齐。
+    int top = top1 > top2 ? top1 : top2;
+    if (top1 != top) {
+        while (top1 < top) {
+            stack1[++top1] = 0;
+        }
+    }
+    if (top2 != top) {
+        while (top2 < top) {
+            stack2[++top2] = 0;
+        }
+    }
+
+    // 从栈中取出数据，实现逆序
     int counter1 = 0;
     while(top1 >= 0) {
         tmp1[counter1++] = stack1[top1--];
@@ -37,36 +54,83 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
     while (top2 >= 0) {
         tmp2[counter2++] = stack2[top2--];
     }
-    int maxCounter = counter1 >= counter2 ? counter1 : counter2;
+
+    int maxCounter = counter1 >= counter2 ? counter1 : counter2; // 2个数相加，结果的位数是其中较大的。
     int bit = 0;
     for (i = maxCounter - 1; i >= 0; i--) {
-        sum[bit] = tmp1[i] + tmp2[i];
-        sum[bit + 1] = sum[bit] / 10;
+        sum[bit] += tmp1[i] + tmp2[i]; // 注意这里的+=。
+        sum[bit + 1] += sum[bit] / 10; //记得进位要累加。
         sum[bit] %= 10;
         bit++;
     }
-
+    
+    //从高位开始，看看数据在哪一个位置。
     for(i = STACK_LENGTH; i >= 0; i--) {
         if (sum[i] != 0) {
             break;
         }
     }
 
+    // 下面将和 sum中的数字放到链表中，注意放的顺序。
     struct ListNode* head = (struct ListNode*)malloc(sizeof(struct ListNode));
     memset(head, 0, sizeof(struct ListNode));
     struct ListNode* resLink = head;
     struct ListNode* p = head;
-    head->val = sum[i];
+    head->val = sum[0];
     head->next = NULL;
-    for (j = 0; j < i; j++) {
+    for (j = 1; j <= i; j++) {
         struct ListNode* tmpList = (struct ListNode*)malloc(sizeof(struct ListNode));
+        tmpList->val = sum[j];
+        tmpList->next = NULL;
         resLink->next = tmpList;
         resLink = tmpList;
     }
+    return head;
+}
 
-    for (j = i; j >= 0; j--) {
-        p->val = sum[j];
-        p = p->next;
+//下面是创建链表
+struct ListNode* CreateLink(int* data, int counter)
+{
+    struct ListNode* head = (struct ListNode*)malloc(sizeof(struct ListNode));
+    head->val = data[0];
+    head->next = NULL;
+
+    struct ListNode* p = NULL;
+    p = head;
+    int i;
+    for (i = 1; i < counter; i++) {
+        struct ListNode* tmp = (struct ListNode*)malloc(sizeof(struct ListNode));
+        tmp->val = data[i];
+        tmp->next = NULL;
+        p->next = tmp;
+        p = tmp;
     }
     return head;
+}
+
+void VistLinkNode(struct ListNode* node)
+{
+    while (node != NULL) {
+        printf("%4d", node->val);
+        node = node->next;
+    }
+}
+
+int main(void)
+{
+    int data1[] = {9, 8};
+    int counter1 = sizeof(data1) / sizeof(data1[0]);
+    struct ListNode* num1 = CreateLink(data1, counter1);
+    VistLinkNode(num1);
+    printf("\n");
+
+    int data2[] = {1};
+    int counter2 = sizeof(data2) / sizeof(data2[0]);
+    struct ListNode* num2 = CreateLink(data2, counter2);
+    VistLinkNode(num2);
+    printf("\n");
+
+    struct ListNode* result = addTwoNumbers(num1, num2);
+    VistLinkNode(result);
+    system("pause");
 }
