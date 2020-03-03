fc8307332e9e05b9c75648b7c73e20c3620bbdc6
lilianglong <lilianglong@nudt.edu.cn>
2020-03-04 00:07:07 +0800

    完善 957 题的解答

--- leetcode_957.c
+++ leetcode_957.c
@@ -1,45 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-/*  Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
-    Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
-    Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
-    Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
-    Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
-    Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
-    Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
-    Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
- */
-
-int JudgeSame(int* a, int* b)
+bool JudgeSame(int* a, int* b)
 {
     for (int i = 0; i < 8; i++) {
         if (a[i] != b[i]) {
-            return 0;
+            return false;
         }
     }
-    return 1;
+    return true;
 }
 
 void Print(int times, int* cells)
 {
-    printf("\r\nday %d: ", times);
+    printf("\r\nday %d:", times);
     for (int i = 0; i < 8; i++) {
         printf("%2d", cells[i]);
     }
     printf("\r\n");
 }
-
-int* prisonAfterNDays(int* cells, int cellsSize, int N, int* returnSize){
-    if (cells == NULL || cellsSize <= 0 || N < 0
-        || returnSize == NULL) {
+int* prisonAfterNDays(int* cells, int cellsSize, int N, 
+    int* returnSize){
+    if (cells == NULL || cellsSize <= 0 || N < 0 || returnSize == NULL) {
         return NULL;
+    }
+    if (N == 0) {
+        *returnSize = cellsSize; 
+        return cells;
     }
     int i;
     int j;
     int tmp[8] = {0};
     int firstOne[8] = {0};
+    int lastOne[8] = {0};
+    bool stop = false;
     for (i = 1; i <= N; i++) {
         for (j = 1; j < cellsSize - 1; j++) {
             tmp[j] = cells[j - 1] == cells[j + 1] ? 1 : 0;
@@ -49,40 +45,55 @@ int* prisonAfterNDays(int* cells, int cellsSize, int N, int* returnSize){
         }
 
         if (i == 1) {
-            memcpy(firstOne, cells, sizeof(int) * 8); // 记录第一天的状态
-        }
-        if (i > 1 && JudgeSame(firstOne, cells)) {
-            printf("\r\nfind the same %d\r\n", i);
-            break;
+            memcpy(firstOne, cells, sizeof(int) * 8); // ��¼��һ���״̬
         }
         Print(i, cells);
+        if (i > 1 && JudgeSame(firstOne, cells)) {
+            printf("\r\nfind the same\r\n");
+            stop = true;
+            break;
+        }
+        memcpy(lastOne, cellsSize, sizeof(int) * cellsSize);
     }
+
+    if (stop == false) {
+        *returnSize = cellsSize;
+        return cells;                
+    }
+
     int day = N % (i - 1);
-    for (i = 0; i < day; i++) {
+    printf("\r\n day=%d N=%d i=%d\r\n", day, N, i);
+    if (day == 0) {
+        memcpy(cells, lastOne, sizeof(int) * cellsSize);
+        *returnSize = cellsSize;
+        return cells;
+    }
+    if (day == 1) {
+        *returnSize = cellsSize;
+        memcpy(cells, firstOne, sizeof(int) * cellsSize);
+        return cells;
+    }
+    for (i = 1; i < day; i++) {
         for (j = 1; j < cellsSize - 1; j++) {
             tmp[j] = cells[j - 1] == cells[j + 1] ? 1 : 0;
         }
-        for (j = 1; j < cellsSize; j++) {
+        for (j = 0; j < cellsSize; j++) {
             cells[j] = tmp[j];
         }
     }
-
     *returnSize = cellsSize;
     return cells;
 }
 
-/*
- * [1,0,0,1,0,0,1,0]
-    1000000000
-    预期输出：[0,0,1,1,1,1,1,0]
- * */
-int main() {
-    int test[] = {1,0,0,1,0,0,1,0};
-    int size = 0;
-    int i;
-    int* result = prisonAfterNDays(test, sizeof(test) / sizeof(test[0]), 17, &size);
-    for (i = 0; i < size; i++) {
+int main(void)
+{
+    int test[] = {0,0,0,1,1,0,1,0};
+    int returnSize = 0;
+    int* result = prisonAfterNDays(test, sizeof(test) / sizeof(test[0]),
+        574, &returnSize);
+    for (int i = 0; i < 8; i++) {
         printf("%2d", result[i]);
     }
-    return 0;
+    printf("\r\n");
+    system("pause");
 }
