f157234c5c5313b30ea8631363994dc4da5099ff
lianglong.li <40571207+leelianglong@users.noreply.github.com>
2020-03-29 11:17:50 +0800

    Create 判断素数的解法.c

---
+++ 总结/判断素数的解法.c
@@ -0,0 +1,27 @@
+// 下面是素数判断的一个
+bool isPrime(int n)
+{
+    if (n == 2 || n == 3) return 1;
+    if (n % 6 != 1 && n % 6 != 5) return 0;
+    
+    for (int i = 5; i <= floor(sqrt(n)); i += 6) {
+        if ((n % i == 0) || (n % (i + 2)) == 0) {
+           return 0;
+         }    
+    }
+            
+    return 1;
+}
+
+下面的方法也可行，但是比较耗时
+bool isPrimeSelf(int n)
+{
+    int i;
+
+    for(i = 2; i <= floor(sqrt(n)); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
